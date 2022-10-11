da33a7f1a655e2129c26ea6575fe6f9b805abf76
Sylvain Desvé <sylvain.desve@gmail.com>
2022-10-11 19:01:49 +0200

    Renommage createApp

--- ddd-todo/packages/ddd-todo-server/src/index.ts
+++ ddd-todo/packages/ddd-todo-server/src/index.ts
@@ -1,7 +1,7 @@
 import { UserId } from "./modules/users/domain";
 import { ServiceRegistry } from "./ServiceRegistry";
 import { InMemoryEventStore } from "./shared/infra/database/InMemoryEventStore";
-import { createApp } from "./shared/infra/http/app";
+import { createApp } from "./shared/infra/http/createApp";
 
 const serviceRegistry = new ServiceRegistry(new InMemoryEventStore<UserId>());
 
